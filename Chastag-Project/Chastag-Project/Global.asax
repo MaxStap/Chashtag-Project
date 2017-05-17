54f9cab17a4ad2e4315017961bda0006886019aa
MaxStap <97043162@st.deltion.nl>
2017-05-17 12:12:22 +0200

    test
    test

---
+++ Chastag-Project/Chastag-Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chastag_Project.MvcApplication" Language="C#" %>
---
+++ Chastag-Project/Chastag-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chastag_Project.Startup))]
+namespace Chastag_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
